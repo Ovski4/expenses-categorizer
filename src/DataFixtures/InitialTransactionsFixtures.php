069348d36852e758c287b448bdd9ef254f217a9f
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2020-03-30 16:11:01 +0200

    Add fixtures for initial transactions

---
+++ src/DataFixtures/InitialTransactionsFixtures.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\SubCategory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\Entity\Transaction;
+use App\Entity\TransactionType;
+
+class InitialTransactionsFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function getDependencies()
+    {
+        return array(
+            SubCategoryFixtures::class,
+        );
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $transactions = [
+            [
+                'label' => 'SOLDE CREDITEUR AU 30/04/2014',
+                'created_at' => new \DateTime('2014-04-30'),
+                'account' => 'C/C EUROCOMPTE JEUNE N° 00020324201',
+                'amount' => 591.73,
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Transfer'
+            ],
+            [
+                'label' => 'SOLDE CREDITEUR AU 01/01/2014',
+                'created_at' => new \DateTime('2014-01-01'),
+                'account' => 'LIVRET JEUNE N° 00020324202',
+                'amount' => 1751.70,
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Transfer'
+            ],
+            [
+                'label' => 'SOLDE CREDITEUR AU 01/05/2014',
+                'created_at' => new \DateTime('2014-05-01'),
+                'account' => 'LIVRET BLEU N° 00020324203',
+                'amount' => 6181.66,
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Transfer'
+            ],
+            [
+                'label' => 'SOLDE CREDITEUR AU 01/05/2014',
+                'created_at' => new \DateTime('2014-01-01'),
+                'account' => 'COMPTE EPARGNE LOGEMENT N° 00020324204',
+                'amount' => 2000,
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Transfer'
+            ],
+            [
+                'label' => 'CLOTURE DE COMPTE AU 28/02/2017',
+                'created_at' => new \DateTime('2017-02-28'),
+                'account' => 'C/C EUROCOMPTE JEUNE N° 00020324201',
+                'amount' => -2766.89,
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Transfer'
+            ],
+            [
+                'label' => 'SOLDE CREDITEUR AU 28/02/2017',
+                'created_at' => new \DateTime('2017-02-28'),
+                'account' => 'Compte Courant JEUNE ACTIF N° 00020324201',
+                'amount' => 2766.89,
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Transfer'
+            ]
+        ];
+
+        foreach ($transactions as $item) {
+            $transaction = new Transaction();
+            $transaction
+                ->setLabel($item['label'])
+                ->setCreatedAt($item['created_at'])
+                ->setAccount($item['account'])
+                ->setAmount($item['amount'])
+            ;
+
+            $subCategory = $manager
+                ->getRepository(SubCategory::class)
+                ->findByNameAndTransactionType($item['subCategoryName'], $item['transactionType'])
+            ;
+            $transaction->setSubCategory($subCategory);
+            $manager->persist($transaction);
+        }
+
+        $manager->flush();
+    }
+}
