c5feb31c83d3752f10be6fa16ae36e3d2ebe7e10
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2020-03-31 16:23:29 +0200

    Add more rules

--- src/DataFixtures/SubCategoryTransactionRuleFixtures.php
+++ src/DataFixtures/SubCategoryTransactionRuleFixtures.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\SubCategoryTransactionRule;
 use App\Entity\TransactionType;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\ORM\NoResultException;
 
 class SubCategoryTransactionRuleFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -73,6 +74,11 @@ class SubCategoryTransactionRuleFixtures extends Fixture implements DependentFix
             ],
             [
                 'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Hosting',
+                'contains' => 'ROUBAIX OVH'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
                 'subCategoryName' => 'Loan',
                 'contains' => 'ECH PRET CAP+IN 07228 203242 05'
             ],
@@ -93,6 +99,11 @@ class SubCategoryTransactionRuleFixtures extends Fixture implements DependentFix
             ],
             [
                 'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Condominium fees',
+                'contains' => 'PRLV SEPA REGIE DE L\'OPERA'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
                 'subCategoryName' => 'Carpooling',
                 'contains' => 'BLABLACAR'
             ],
@@ -100,17 +111,201 @@ class SubCategoryTransactionRuleFixtures extends Fixture implements DependentFix
                 'transactionType' => TransactionType::REVENUES,
                 'subCategoryName' => 'Payback',
                 'contains' => 'BLABLACAR'
-            ]
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Payback',
+                'contains' => 'VIR SEPA LORAINE FRIOUX'
+            ],
+            [
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Transfer',
+                'contains' => 'VIR DE M BAPTISTE BOUCHEREAU'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Transfer',
+                'contains' => 'VIR COMPTE COURANT JEUNE ACTIF'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Supermarket and groceries',
+                'contains' => 'ANGERS CEDEX GEANT'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Gasoline',
+                'contains' => 'SAUGON ESSO SAUGON EST'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Restaurant',
+                'contains' => 'BAYONNE REST CHEZ BAI'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Restaurant',
+                'contains' => 'ANGERS LE TEMPLE DU CIE'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Restaurant',
+                'contains' => 'ST JEAN DE LU LE MAJESTIC'
+            ],
+            [
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Payback',
+                'contains' => 'VIR GIEPS- SIN ET COM'
+            ],
+            [
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Payback',
+                'contains' => 'VIR CPAM LOIRE'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Restaurant',
+                'contains' => 'NANTES MAISON GRIMAUD'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Drinks',
+                'contains' => 'NANTES CLUB DE L ILE'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Restaurant',
+                'contains' => 'REZE LA GOURMANDE'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Doctor, medical centre',
+                'contains' => 'ANGERS DR SEGRETAIN'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Video games',
+                'contains' => 'WWW.STEAMPOWERED'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Pharmacy',
+                'contains' => 'ANGERS PHIE DE LORETTE'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Supermarket and groceries',
+                'contains' => 'ANGERS CARREFOUR EXPRES'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Transfer',
+                'contains' => 'VIR LIVRET BLEU'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Train',
+                'contains' => 'SNCF OUIGO'
+            ],
+            [
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Rent',
+                'contains' => 'VIR MR OU MME LE TRAOU DOMIN'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Supermarket and groceries',
+                'contains' => 'BIOCOOP'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Gasoline',
+                'contains' => 'ANGERS DAC TIMAEL'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Fast food',
+                'contains' => 'BURGER KING'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Sport field',
+                'contains' => 'LES PONTS DE CLIMB UP'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Bank fee',
+                'contains' => 'F COTIS EUC JEUNE ACTIF'
+            ],
+            [
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Rent',
+                'contains' => 'VIR MLLE NATHALIE ROCHE'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Sport field',
+                'contains' => 'NANTES SMASH GOAL'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Bakery',
+                'contains' => 'NANTES AU PAIN GOURMAND'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Bakery',
+                'contains' => 'SAINT JULIEN LES SAVEURS D AN'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Train',
+                'contains' => 'PARIS TRAINLINE'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Tramway or subway',
+                'contains' => 'PARIS IRIGO'
+            ],
+            [
+                'transactionType' => TransactionType::REVENUES,
+                'subCategoryName' => 'Salary',
+                'contains' => 'VIR SARL IDCI CONSULTING'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Bakery',
+                'contains' => 'ANGERS MAISON LATAIRE'
+            ],
+            [
+                'transactionType' => TransactionType::EXPENSES,
+                'subCategoryName' => 'Train',
+                'contains' => 'THETRAINLINE.COM'
+            ],
+            // [
+            //     'transactionType' => TransactionType::EXPENSES,
+            //     'subCategoryName' => 'XXXXXX',
+            //     'contains' => 'LONDON GOOGLE GOOGLE'
+            //     'amount' => [
+            //       'operator' => 'equal',
+            //       'value'    => -9.99
+            //     ]
+            // ]
         ];
 
         foreach ($subCategoryTransactionRuleArray as $item) {
             $subCategoryTransactionRule = new SubCategoryTransactionRule();
             $subCategoryTransactionRule->setContains($item['contains']);
 
-            $subCategory = $manager
-                ->getRepository(SubCategory::class)
-                ->findByNameAndTransactionType($item['subCategoryName'], $item['transactionType'])
-            ;
+            try {
+                $subCategory = $manager
+                    ->getRepository(SubCategory::class)
+                    ->findByNameAndTransactionType($item['subCategoryName'], $item['transactionType'])
+                ;
+            } catch (NoResultException $e) {
+                throw new \Exception(sprintf('No subCategory found with name %s', $item['subCategoryName']));
+            }
+
             $subCategoryTransactionRule->setSubCategory($subCategory);
             $manager->persist($subCategoryTransactionRule);
         }
