2c4835010a500b75257533aaaf4966a54b502f3b
Baptiste Bouchereau <baptiste.bouchereau@pixieset.com>
2022-09-18 14:22:14 -0700

    Use spaces instead of tabs

--- src/Entity/Settings.php
+++ src/Entity/Settings.php
@@ -11,15 +11,15 @@ class Settings
     const NAME_LAST_PDF_STATEMENT_PARSER_USED = 'last_parser_used';
     const NAME_LAST_CSV_PARSER_USED = 'last_csv_parser_used';
 
-	#[ORM\Id]
+    #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     protected ?int $id = null;
 
-	#[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
     protected  ?string $name = null;
 
-	#[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255)]
     protected ?string $value = null;
 
     public function getId(): ?int
